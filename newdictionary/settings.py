9fe12ce7d2c892bd7852e3074d00dcd9cce757eb
Farhaan Khan <farhaankhanfiroz@gmail.com>
2021-11-13 12:04:24 +0530

    Tweaked app for heroku

--- newdictionary/settings.py
+++ newdictionary/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'newdictionary.urls'
@@ -126,7 +129,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
--- webapp/views.py
+++ webapp/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from PyDictionary import PyDictionary
+from nltk.corpus import wordnet
 
 # Create your views here.
 def home(request):
-    return render(request, 'webapp/Page-1.html', {})
+    return render(request, 'webapp/index.html', {})
 
 def meaning(request):
     word = request.GET.get('word')
     dictionary = PyDictionary()
     mean = dictionary.meaning(word)
-    synonyms = dictionary.synonym(word)
-    antonyms = dictionary.antonym(word)
+    synonyms = []
+    antonyms = []
+
+    for syn in wordnet.synsets(word):
+        for lm in syn.lemmas():
+            if lm.name() != word.lower():
+                synonyms.append(lm.name())
+
+    for syn in wordnet.synsets(word):
+        for lm in syn.lemmas():
+            if lm.antonyms():
+                antonyms.append(lm.antonyms()[0].name())
 
     if mean == None:
         word = "Word not found"
@@ -24,18 +35,18 @@ def meaning(request):
     else:
         mean = "Meaning : {}.".format(mean['Adverb'][0])
     
-    if synonyms == None:
+    if len(synonyms) == 0:
         synonyms = "Synonyms :"
     elif len(synonyms) > 1:
         synonyms = "Synonyms : {}, {}.".format(synonyms[0], synonyms[1])
     else:
         synonyms = "Synonyms : {}.".format(synonyms[0])
     
-    if antonyms == None:
+    if len(antonyms) == 0:
         antonyms = "Antonyms :"
     elif len(antonyms) > 1:
         antonyms = "Antonyms : {}, {}.".format(antonyms[0], antonyms[1])
     else:
         antonyms = "Antonyms : {}.".format(antonyms[0])
 
-    return render(request, 'webapp/Page-2.html', {'word': word,'meaning': mean,'synonyms': synonyms,'antonyms': antonyms})
+    return render(request, 'webapp/dictionary.html', {'word': word,'meaning': mean,'synonyms': synonyms,'antonyms': antonyms})
